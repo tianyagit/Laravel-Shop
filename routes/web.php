9f6bd9f8d471191ee0dcbd151714454b9242f122
tianyamike <1974588993@qq.com>
2020-09-27 15:29:33 +0800

    修改和删除收货地址

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,20 @@ Auth::routes(['verify' => true]);
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function(){
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+
     Route::get('user_addresses/create', 'UserAddressesController@create')
         ->name('user_addresses.create');
+
     Route::post('user_addresses', 'UserAddressesController@store')
         ->name('user_addresses.store');
+
+    Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')
+        ->name('user_addresses.edit');;
+
+    Route::put('user_addresses/{user_address}', 'UserAddressesController@update')
+        ->name('user_addresses.update');
+
+    Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')
+        ->name('user_addresses.destroy');
+
 });

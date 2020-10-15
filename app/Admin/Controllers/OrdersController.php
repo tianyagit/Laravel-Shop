a63374627d565c1ea624e504f1fc53df133c159f
tianyamike <1974588993@qq.com>
2020-10-15 17:04:02 +0800

    管理后台 - 拒绝退款

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -13,6 +13,7 @@ use App\Exceptions\InvalidRequestException;
 use Illuminate\Http\Request;
 
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use App\Http\Requests\Admin\HandleRefundRequest;
 
 class OrdersController extends AdminController
 {
@@ -106,6 +107,31 @@ class OrdersController extends AdminController
 
             // 返回上一页
             return redirect()->back();
+    }
+
+    public function handleRefund(Order $order, HandleRefundRequest $request)
+    {
+        // 判断订单状态是否正确
+        if($order->refund_status !== Order::REFUND_STATUS_APPLIED) {
+            throw new InvalidRequestException('订单状态不正确');
+        }
+
+        //  是否同意退款
+        if ($request->input('agree')) {
+
+        } else {
+            // 将拒绝退款理由放到订单的 extra 字段中
+            $extra = $order->extra ?: [];
+            $extra['refund_disagree_reason'] = $request->input('reason');
+
+            // 将订单的退款状态改为未退款
+            $order->update([
+                'refund_status' => Order::REFUND_STATUS_PENDING,
+                'extra' => $extra
+            ]);
+        }
+
+        return $order;
     }
 
 

e197100af32194435e9f7b4d5ee5cae7da87950d
tianyamike <1974588993@qq.com>
2020-10-20 15:24:29 +0800

    导出管理后台数据

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
+        $this->call(AdminTablesSeeder::class);
+        $this->call(UsersSeeder::class);
         $this->call(UserAddressesSeeder::class);
         $this->call(ProductsSeeder::class);
         $this->call(CouponCodesSeeder::class);
